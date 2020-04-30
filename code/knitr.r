8e4b05449b1edebc64e5bb998c6cb98020f82649
Edvinas <edvinas.karenga@softemia.lt>
2020-04-30 20:52:58 +0300

    Change outlier removal to use 3 standard deviation cut-off. Remove both sample from same subject if only one is left. Add principal component analysis.

--- code/knitr.r
+++ code/knitr.r
@@ -3,3 +3,4 @@ output_dir <- "./reports"
 render("./code/data_read.Rmd", output_dir = output_dir)
 render("./code/data_preprocessing.Rmd", output_dir = output_dir)
 render("./code/outlier_removal.Rmd", output_dir = output_dir)
+render("./code/PCA.Rmd", output_dir = output_dir)
